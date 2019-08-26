536b8d2be569d35730f457577562f01dd593e293
陈熠豪 <chenyiha17@mails.tsinghua.edu.cn>
2019-08-26 23:00:05 +0800

    add basic fragments for UI

---
+++ app/src/main/java/com/example/newsclientapp/ui/fragment/FragmentFactory.java
@@ -0,0 +1,26 @@
+package com.example.newsclientapp.ui.fragment;
+
+import java.util.HashMap;
+
+public final class FragmentFactory {
+    public enum FragmentEnum {
+        NEWS_TAB_FRAGMENT
+    }
+
+    private static HashMap<FragmentEnum, Class<? extends BaseFragment>> fragmentClassMap;
+
+    static {
+        fragmentClassMap = new HashMap<>();
+        fragmentClassMap.put(FragmentEnum.NEWS_TAB_FRAGMENT, NewsTabFragment.class);
+    }
+
+    public static Class<? extends BaseFragment> getFragmentClass(FragmentEnum fIndex) {
+        // return null if not exists
+        return fragmentClassMap.get(fIndex);
+    }
+
+    public static BaseFragment getFragmentInstance(FragmentEnum fIndex) throws Exception {
+        // no check
+        return fragmentClassMap.get(fIndex).getConstructor().newInstance();
+    }
+}
