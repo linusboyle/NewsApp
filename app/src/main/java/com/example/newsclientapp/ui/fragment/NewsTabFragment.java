4270a1042f4ffd0e40be43b679c556afafef4d2c
陈熠豪 <chenyiha17@mails.tsinghua.edu.cn>
2019-08-30 19:46:38 +0800

    add tab choose page

--- app/src/main/java/com/example/newsclientapp/ui/fragment/NewsTabFragment.java
+++ app/src/main/java/com/example/newsclientapp/ui/fragment/NewsTabFragment.java
@@ -1,8 +1,12 @@
 package com.example.newsclientapp.ui.fragment;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
@@ -17,10 +21,13 @@ import java.util.List;
 import butterknife.BindView;
 
 public class NewsTabFragment extends BaseFragment {
-    @BindView(R.id.tab_layout)
-    TabLayout mTabLayout;
-    @BindView(R.id.view_pager)
-    ViewPager mViewPager;
+    private static final int PRIMARY_COLOR = 0xFFBB86FC;
+    private static final int TEXT_COLOR = 0xFFCECECE;
+
+    @BindView(R.id.tab_function) ImageView mTabFunc;
+    @BindView(R.id.tab_layout) TabLayout mTabLayout;
+    @BindView(R.id.view_pager) ViewPager mViewPager;
+    @BindView(R.id.tab_choose_scroll) NestedScrollView mTabChoose;
 
     @Override
     protected int getLayoutId() {
@@ -38,5 +45,54 @@ public class NewsTabFragment extends BaseFragment {
         mViewPager.setCurrentItem(0);
         mViewPager.setOffscreenPageLimit(fragmentList.size() - 1);
         mTabLayout.setupWithViewPager(mViewPager);
+        mTabChoose.setVisibility(View.GONE);
+
+        mTabFunc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                // TODO
+                if (mViewPager.getVisibility() == View.VISIBLE) {
+                    mTabFunc.setColorFilter(PRIMARY_COLOR);
+                    mViewPager.setVisibility(View.GONE);
+                    mTabChoose.setVisibility(View.VISIBLE);
+                    setTabsEnabled(false);
+
+                } else {
+                    mTabFunc.setColorFilter(TEXT_COLOR);
+                    mViewPager.setVisibility(View.VISIBLE);
+                    mTabChoose.setVisibility(View.GONE);
+                    setTabsEnabled(true);
+                }
+            }
+        });
+    }
+
+    private ViewGroup getTabViewGroup() {
+        ViewGroup viewGroup = null;
+        if (mTabLayout.getChildCount() > 0) {
+            View view = mTabLayout.getChildAt(0);
+            if (view instanceof ViewGroup)
+                viewGroup = (ViewGroup) view;
+        }
+        return viewGroup;
+    }
+
+    private void setTabsEnabled(boolean enable) {
+        ViewGroup viewGroup = getTabViewGroup();
+        if (viewGroup != null)
+            for (int childIndex = 0; childIndex < viewGroup.getChildCount(); childIndex++) {
+                View tabView = viewGroup.getChildAt(childIndex);
+                if (tabView != null)
+                    tabView.setEnabled(enable);
+            }
+    }
+
+    private View getTabView(int position) {
+        View tabView = null;
+        ViewGroup viewGroup = getTabViewGroup();
+        if (viewGroup != null && viewGroup.getChildCount() > position)
+        	tabView = viewGroup.getChildAt(position);
+        return tabView;
     }
 }
+
